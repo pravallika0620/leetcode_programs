d2e1557f7895926af7ed56881c949cbdc1a6a86c
pravallika0620 <pravallikapedapatruni25@gmail.com>
2025-09-18 20:33:22 +0530

    [1] [Time Beats: 69.98%] [Memory Beats: 36.04%] - LeetPush

---
+++ Two-Sum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> num_map; 
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (num_map.find(complement) != num_map.end()) {
+            return {num_map[complement], i}; 
+        }
+        num_map[nums[i]] = i; 
+    }
+    return {}; 
+}
+
+int main() {
+    vector<int> nums ;
+    int target ;
+    for(int i=0;i>=0;i++)
+    {
+       cin>>nums[i];
+    }
+    cin>>target;
+    vector<int> result = twoSum(nums, target);
+
+    cout << " [" << result[0] << ", " << result[1] << "]" << endl;
+
+    return 0;
+}
+};
